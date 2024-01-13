9b849c639d7556247972bd6c7f3eb27ee3dc205b
rumert <154933062+rumert@users.noreply.github.com>
2024-01-13 22:58:24 +0300

    added api

--- client/src/app/(afterLogin)/chats/page.tsx
+++ client/src/app/(afterLogin)/chats/page.tsx
@@ -1,24 +1,39 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+interface MessageObject {
+  sender: string;
+  text: string;
+}
 
 export default function Chats() {
 
   const [message, setMessage] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<MessageObject[]>([])
 
+  useEffect ( () => {
+    async function fetchData() {
+      setIsLoading(true)
+      const res = await fetch('/api/messages')
+      const initialData = await res.json()
+      setMessages(initialData.messages)
+      setIsLoading(false)
+    }
+    fetchData()
+  }, []) 
+  
   const handleSubmit = async (e: any) => {
     e.preventDefault()
-    setIsLoading(true)
     if (message.trim() !== '') {
       const res = await fetch('/api/messages', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({ message })
+      body: JSON.stringify( message )
       })
     }
-    setIsLoading(false)
+    console.log('data sent!')
   }
-  
   return (
     <div className='flex-grow flex flex-col'>
         <div className='h-[11vh] border-2 rounded-2xl flex gap-2 pl-4 items-center'>
@@ -26,7 +41,12 @@ export default function Chats() {
           <div className='h-[10vh] w-[10vh] bg-gray-800 rounded-full'>b</div>
         </div> {/* chats navbar */}
         <div className='flex-grow flex flex-col'>
-          <div className='flex-grow'></div> {/* conversation */}
+          <div className='flex-grow flex flex-col gap-1'>
+            {messages.length != 0 && 
+              messages.map( (m: MessageObject, index: number) => {
+                return <div key={index}>{m.text}</div>
+            })}
+          </div> {/* conversation */}
           <form onSubmit={handleSubmit} className='h-[15%] flex'>
             <input 
               type="text" 
--- client/src/app/api/messages/route.ts
+++ client/src/app/api/messages/route.ts
@@ -1,5 +1,43 @@
+import { db } from "@/app/firebase"
+import { arrayUnion, doc, updateDoc, getDoc, onSnapshot} from "firebase/firestore"; 
+
+const chatRef = doc(db, "chats", "r5vcum1IsYJaDR6raOKK")
 
 export async function POST(req: Request) {
-    const message = await req.json()
-    return Response.json({ message })
+    
+    const text = await req.json()
+
+    try {
+        const message = {
+            sender: "sender",
+            text: text
+        }
+        await updateDoc(chatRef, {
+            messages: arrayUnion(message)
+        });
+    } catch (err) {
+      console.error('firebase error:', err);
+    }
+    
+    return Response.json('OK') 
 }
+
+export async function GET() {
+    const docSnap = await getDoc(chatRef)
+    const data = docSnap.data()
+    return Response.json( data )
+}
+
+const changedData = () => onSnapshot(chatRef, (snapshot) => {
+    console.log('data received: ', snapshot)
+    let chats: any = []
+    const dataa: any = snapshot.data()
+    const messagess = dataa.messages
+    messagess.map((message: any) => chats.push(message.text))
+    
+    return () => changedData()
+    //setMessages(doc.data().messages);
+})
+
+
+
