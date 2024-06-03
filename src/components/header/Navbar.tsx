35e5b2d90a57f64ef07c8f695b69f60d8c0cb2e5
rumert <154933062+rumert@users.noreply.github.com>
2024-06-03 06:39:43 +0300

    first touches

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,21 +1,16 @@
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
-function Navbar() {
+export default function Navbar() {
 
   return (
-    <div className="navbar bg-neutral text-neutral-content px-10 max-w-[1024px] mx-auto rounded-2xl m-2">
-      <div className="flex-1">
-        <Link href="/" className="hover:text-primary">
-          NEXTCHAT
-        </Link> 
-      </div>
-      <div className="flex-none">
-        <Link href="/login" className="btn btn-primary">
-          REGISTER
-        </Link> 
-      </div>
-    </div>
+    <nav className="w-10/12 max-w-[1024px] mx-auto rounded-lg flex justify-between items-center mt-2 mb-12 py-2 px-8 bg-secondary">
+      <Link href="/" className='className="text-lg font-semibold hover:text-primary'>
+        nextchat
+      </Link>
+      <Button className='text-lg' asChild>
+        <Link href="/login">Login</Link>
+      </Button>
+    </nav>
   )
 }
-
-export default Navbar
